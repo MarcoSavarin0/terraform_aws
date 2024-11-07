388f3dda97ca9da5a7e3583e8c027efc411c37cd
MarcoSavarin0 <ateneo2003@gmail.com>
2024-11-06 23:00:04 -0300

    se asocia ip publica a la de prod

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,18 @@
+
 resource "aws_instance" "testing" {
   ami               = "ami-0866a3c8686eaeeba"
   instance_type     = "t2.micro"
   availability_zone = "us-east-1a"
   key_name          = "terraform"
-  subnet_id         = "subnet-0f8e6f79d6b379e15"
+  subnet_id         = module.vpc.private_subnets[0]
+  depends_on        = [module.vpc]
 }
 resource "aws_instance" "produccion" {
   ami               = "ami-0866a3c8686eaeeba"
   instance_type     = "t2.micro"
   availability_zone = "us-east-1b"
   key_name          = "terraform"
-  subnet_id         = "subnet-00500a62d6e43c189"
+  subnet_id         = module.vpc.public_subnets[1]
+  depends_on        = [module.vpc]
+  associate_public_ip_address = true
 }
--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,11 @@
 module "vpc" {
-  source          = "terraform-aws-modules/vpc/aws"
-  version         = "5.5.3"
-  name            = "vpc-dev"
-  cidr            = "10.10.0.0/16"
-  azs             = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.10.1.0/24", "10.10.2.0/24"]
-  private_subnets = ["10.10.3.0/24", "10.10.4.0/24"]
+  source                  = "terraform-aws-modules/vpc/aws"
+  version                 = "5.5.3"
+  name                    = "vpc-Marco-tf"
+  cidr                    = "10.10.0.0/16"
+  azs                     = ["us-east-1a", "us-east-1b"]
+  public_subnets          = ["10.10.1.0/24", "10.10.2.0/24"]
+  private_subnets         = ["10.10.3.0/24", "10.10.4.0/24"]
+  enable_nat_gateway      = true 
+  map_public_ip_on_launch = true 
 }
